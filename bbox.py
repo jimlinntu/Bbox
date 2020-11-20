fce6e1969dcf568a51887aea05bfd0ed621c45c9
Jim Lin <b04705003@ntu.edu.tw>
2020-11-20 11:01:07 +0800

    add crop_image function

--- bbox.py
+++ bbox.py
@@ -36,3 +36,40 @@ class Bbox():
         bbox = np.roll(bbox, 4 - startidx, 0)
         return bbox
 
+    def crop_image(self, img):
+        '''
+            Case 1: Normal case
+                           w
+                [0]------------------[1]
+                 |                    |
+                 |   1  2  3  4  5    |  h
+                 |                    |
+                [3]------------------[2]
+
+            Case 2:
+                     w
+                [0]-----[1]
+                 |   5   |
+                 |   4   |
+                 |   3   | h
+                 |   2   |
+                 |   1   |
+                [3]-----[2]
+        '''
+        bbox = self.get_reordered_bbox()
+        w, h = int(np.linalg.norm(bbox[0] - bbox[1])), int(np.linalg.norm(bbox[0] - bbox[3]))
+
+        if h < w * 1.5:
+            # Case 1
+            dst = np.array([[0, 0], [w, 0], [w, h], [0, h]], dtype=np.float32)
+            M = cv2.getPerspectiveTransform(bbox.astype(np.float32), dst)
+            crop_width, crop_height = w, h
+        else:
+            # Case 2
+            dst = np.array([[h, 0], [h, w], [0, w], [0, 0]], dtype=np.float32)
+            M = cv2.getPerspectiveTransform(bbox.astype(np.float32), dst)
+            crop_width, crop_height = h, w
+
+        cropped = cv2.warpPerspective(img, M, (crop_width, crop_height))
+        return cropped
+
